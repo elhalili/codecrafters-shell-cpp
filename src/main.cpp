c09ff99528536f8fbf423b366888c5a8b60b6180
elhalili <amine.elhalili1@gmail.com>
2025-06-16 12:32:41 +0100

    Update structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,10 @@
 #include <iostream>
 #include <string>
+#include "aeshell/shell.h"
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-
-  // Uncomment this block to pass the first stage
-  std::cout << "$ ";
-
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout << input << ": command not found" << std::endl;
-
+  aeshell::Shell shell;
+  shell.Run();
 
   return 0;
 }
